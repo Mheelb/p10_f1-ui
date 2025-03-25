891b6c8ed63e7605c2dbdb3e38311a67f3e1bb5c
Mheelb <maatteoo.h@gmail.com>
2025-03-25 15:43:23 +0100

    add grand prix info and left timer

--- src/components/BetCard.tsx
+++ src/components/BetCard.tsx
@@ -1,7 +1,10 @@
 'use client';
 
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import Button from '@/components/common/Button';
+import { getCode } from 'country-list';
+import Flag from 'react-world-flags'
+import parse from 'html-react-parser';
 
 const BetCard: FC = () => {
 
@@ -34,13 +37,52 @@ const BetCard: FC = () => {
         },
         distance: "308.5 Kms",
         timezone: "utc",
-        date: "2023-03-05T15:00:00+00:00",
+        date: "2025-03-29T07:00:00Z",
         weather: null,
         status: "Completed",
     }
+
+    const countryName = fakeNextRace.competition.location.country;
+    const countryCode = getCode(fakeNextRace.competition.location.country);
+    const grandPrixName = fakeNextRace.competition.name;
+    const circuitImage = fakeNextRace.circuit.image;
+    const date = new Date(fakeNextRace.date);
+
+    const getTimeLeft = () => {
+        const now = new Date();
+        const diff = date.getTime() - now.getTime();
+        const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+        return parse(`<h1>${days}</h1><p>d :&nbsp;</p><h1>${hours}</h1><p>h :&nbsp;</p><h1>${minutes}</h1><p>m</p>`);
+    }
+
+    const [timeLeft, setTimeLeft] = useState(getTimeLeft());
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setTimeLeft(getTimeLeft());
+        }, 1000);
+
+        return () => clearInterval(timer);
+    }, []);
     
     return (
         <div className='bet-card w-90 '>
+            <div className='grand-prix-info flex justify-between'>
+                <div>
+                    <div className='flex'>
+                        <Flag code={countryCode} className="w-8 h-8 mr-2" />
+                        <h1>{countryName}</h1>
+                    </div>
+                    <p>{grandPrixName}</p>
+                </div>
+                <img src={circuitImage} alt="circuit" className='w-20 h-20' />
+            </div>
+            <div className='timer'>
+                <div className='flex items-center'>{timeLeft}</div>
+            </div>
             <Button onClick={() => console.log('clicked')}>Bet</Button>
         </div>
     )
