0967c25859f62f71626c67573ed0e19e4dee3995
Mheelb <maatteoo.h@gmail.com>
2025-03-25 16:05:54 +0100

    refactor timer to component

--- src/components/BetCard.tsx
+++ src/components/BetCard.tsx
@@ -4,7 +4,7 @@ import { FC, useEffect, useState } from 'react';
 import Button from '@/components/common/Button';
 import { getCode } from 'country-list';
 import Flag from 'react-world-flags'
-import parse from 'html-react-parser';
+import Timer from '@/components/common/Timer';
 
 const BetCard: FC = () => {
 
@@ -47,26 +47,6 @@ const BetCard: FC = () => {
     const grandPrixName = fakeNextRace.competition.name;
     const circuitImage = fakeNextRace.circuit.image;
     const date = new Date(fakeNextRace.date);
-
-    const getTimeLeft = () => {
-        const now = new Date();
-        const diff = date.getTime() - now.getTime();
-        const days = Math.floor(diff / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((diff % (1000 * 60)) / 1000);
-        return parse(`<h1>${days}</h1><p>d :&nbsp;</p><h1>${hours}</h1><p>h :&nbsp;</p><h1>${minutes}</h1><p>m</p>`);
-    }
-
-    const [timeLeft, setTimeLeft] = useState(getTimeLeft());
-
-    useEffect(() => {
-        const timer = setInterval(() => {
-            setTimeLeft(getTimeLeft());
-        }, 1000);
-
-        return () => clearInterval(timer);
-    }, []);
     
     return (
         <div className='bet-card w-90 '>
@@ -80,10 +60,7 @@ const BetCard: FC = () => {
                 </div>
                 <img src={circuitImage} alt="circuit" className='w-20 h-20' />
             </div>
-            <div className='timer-box flex justify-between items-center'>
-                <p>Time left to bet</p>
-                <div className='flex items-center timer'>{timeLeft}</div>
-            </div>
+            <Timer dateCircuit={date} />
             <Button onClick={() => console.log('clicked')}>Bet</Button>
         </div>
     )
