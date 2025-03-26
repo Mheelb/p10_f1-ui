2fcf8744da35276d480fc687a03ef5a8ce0207cf
Mheelb <maatteoo.h@gmail.com>
2025-03-26 20:36:45 +0100

    fix hydration error

--- src/components/BetCard.tsx
+++ src/components/BetCard.tsx
@@ -3,32 +3,35 @@
 import { FC, useEffect, useState } from 'react';
 import Button from '@/components/common/Button';
 import { getCode } from 'country-list';
-import Flag from 'react-world-flags'
+import Flag from 'react-world-flags';
 import Timer from '@/components/common/Timer';
 import { faker } from '@faker-js/faker';
 
 const BetCard: FC = () => {
-
-    //faker
-    function generateNextRace() {
-        return {
-          id: faker.number.int(),
-          competition: {
-            name: faker.lorem.words(3),
-            country: faker.location.country(),
-          },
-          circuit: {
-            name: faker.location.streetAddress(),
-            image: faker.image.urlLoremFlickr({ category: 'sports' }),
-          },
-          date: faker.date.future().toISOString(),
-        };
-      }
-
     const hasBet = false;
-
     const [bet, setBet] = useState(getBet());
-    const [nextRace, setNextRace] = useState(generateNextRace());
+    const [nextRace, setNextRace] = useState<any>(null);
+
+    useEffect(() => {
+        setNextRace({
+            id: faker.number.int(),
+            competition: {
+                name: faker.lorem.words(3),
+                country: faker.location.country(),
+            },
+            circuit: {
+                name: faker.location.streetAddress(),
+                image: faker.image.urlLoremFlickr({ category: 'sports' }),
+            },
+            date: faker.date.future().toISOString(),
+        });
+    }, []);
+
+    function getBet() {
+        return hasBet ? 'Hamilton' : '';
+    }
+
+    if (!nextRace) return null;
 
     const countryName = nextRace.competition.country;
     const countryCode = getCode(nextRace.competition.country);
@@ -36,15 +39,8 @@ const BetCard: FC = () => {
     const circuitImage = "https://media.api-sports.io/formula-1/circuits/29.png";
     const date = new Date(nextRace.date);
 
-    function getBet() {
-        if (hasBet)
-            return 'Hamilton';
-        else
-            return '';
-    };
-
     return (
-        <div className='bet-card w-90 '>
+        <div className='bet-card w-90'>
             <div className='grand-prix-info flex justify-between'>
                 <div>
                     <div className='flex'>
@@ -57,14 +53,16 @@ const BetCard: FC = () => {
             </div>
             <Timer dateCircuit={date} />
             {hasBet ? (
-                <div >
-                    <h2>your bet :</h2>
+                <div>
+                    <h2>Your bet:</h2>
                     <h1 className='mb-2'>{bet}</h1>
                     <Button onClick={() => console.log('clicked')} color="secondary">Change</Button>
                 </div>
-                ) : <Button onClick={() => console.log('clicked')}>Bet</Button>}
+            ) : (
+                <Button onClick={() => console.log('clicked')}>Bet</Button>
+            )}
         </div>
-    )
-}
+    );
+};
 
 export default BetCard;
