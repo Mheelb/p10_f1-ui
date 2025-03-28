118048970e7763bee173fda97124717558302f45
Mheelb <maatteoo.h@gmail.com>
2025-03-28 11:33:08 +0100

    remove comments

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,15 +1,15 @@
 import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
-  testDir: './tests/e2e',  // Dossier où sont stockés les tests E2E
-  timeout: 30000,          // Temps d'attente maximum pour chaque test
-  retries: 2,              // Nombre de réessais en cas d'échec
-  workers: 4,              // Nombre de workers (tests parallèles)
+  testDir: './tests/e2e',
+  timeout: 30000,
+  retries: 2,
+  workers: 4,
   use: {
-    headless: true,       // Lancer les tests en mode non-headless
-    screenshot: 'on-failure',  // Prendre une capture d'écran en cas d'échec
-    video: 'on-first-retry',  // Enregistrer une vidéo du test lors du premier échec
-    baseURL: 'http://host.docker.internal:3000',  // L'URL de base de ton app
+    headless: true,
+    screenshot: 'on-failure',
+    video: 'on-first-retry',
+    baseURL: 'http://host.docker.internal:3000',
     outputDir: './playwright-report',
   },
   projects: [
