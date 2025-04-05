33538fec54e38656d6b1ac9606df70630ecd445b
Mheelb <maatteoo.h@gmail.com>
2025-04-05 17:49:06 +0200

    remove all auth popup on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,11 @@
 "use client";
 
-import AuthPopup from '@/components/AuthPopup';
 import BetCard from '@/components/BetCard';
-import { useState, useEffect } from 'react';
-import Button from '@/components/common/Button';
 
 export default function Home() {
-  const [isPopupVisible, setIsPopupVisible] = useState(false);
-  const togglePopup = () => {
-    setIsPopupVisible((prev) => !prev);
-  };
-
-  useEffect(() => {
-    if (isPopupVisible) {
-      document.body.style.overflow = "hidden";
-    } else {
-      document.body.style.overflow = "auto";
-    }
-
-    return () => {
-      document.body.style.overflow = "auto";
-    };
-  }, [isPopupVisible]);
-  
   return (
     <div className="flex flex-col justify-center items-center h-screen bg-image">
       <BetCard />
-      <AuthPopup isVisible={isPopupVisible} togglePopup={togglePopup}/>
     </div>
   );
 }
