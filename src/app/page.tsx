da89aae49af24498e45793208d854c1eeedb1375
Mheelb <maatteoo.h@gmail.com>
2025-04-05 17:48:05 +0200

    remove test auth popup button on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,6 @@ export default function Home() {
     <div className="flex flex-col justify-center items-center h-screen bg-image">
       <BetCard />
       <AuthPopup isVisible={isPopupVisible} togglePopup={togglePopup}/>
-      <Button onClick={togglePopup} width="40">Open Auth Popup</Button>
     </div>
   );
 }
