a0a812f230bdd424c31a1735c4c9c59cee8228a9
Athénaïs GRAVIL <agravil@MacBook-Air-de-Athenais.local>
2025-04-09 21:35:00 +0200

    change color, add active

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -6,50 +6,46 @@ import { GiCheckeredFlag } from "react-icons/gi";
 import { GoTrophy } from "react-icons/go";
 import { MdOutlineHowToVote } from "react-icons/md";
 import { GiCharacter } from "react-icons/gi";
-
+import { useRouter } from 'next/navigation';
+import { usePathname } from "next/navigation";
 
 interface FooterProps {}
 
 const Footer: FC<FooterProps> = () =>{
-  
-  
+    const router = useRouter();
+    const curentPath = usePathname();
+
+    const isActive = (path: string) => {
+        return curentPath === path ? 'text-red-500 hover:cursor-pointer' : 'text-black hover:cursor-pointer';
+    }
+
     return (
-    <footer className="fixed bottom-0 right-0 left-0 w-full">
-        <div className="flex items-center justify-between w-full h-16 bg-red-500 text-white px-4">
+    <footer className="fixed bottom-0 right-0 left-0 w-full border-t border-red-200">
+        <div className="flex items-center justify-between w-full h-16 bg-white px-4 ">
             <IoHomeOutline
-                className="hover:cursor-pointer"
+                className={isActive('/')}
                 size={20}
-                onClick={() => {
-                    window.location.href = "/";
-                }}
+                onClick={() => router.push('/')}
             />
             <GoTrophy
-                className="hover:cursor-pointer"
+                className={isActive('/leagues')}
                 size={20}
-                onClick={() => {
-                    window.location.href = "/leagues";
-                }}
+                onClick={() => router.push('/leagues')}
             />
             <GiCheckeredFlag 
-                className="hover:cursor-pointer"
+                className={isActive('/races')}
                 size={25}
-                onClick={() => {
-                    window.location.href = "/races";
-                }}
+                onClick={() => router.push('/races')}
             />
             <MdOutlineHowToVote 
-                className="hover:cursor-pointer"
+                className={isActive('/vote')}
                 size={20}
-                onClick={() => {
-                    window.location.href = "/vote";
-                }}
+                onClick={() => router.push('/vote')}
             />
             <GiCharacter 
-                className="hover:cursor-pointer"
+                className={isActive('/account')}
                 size={20}
-                onClick={() => {
-                    window.location.href = "/account";
-                }}
+                onClick={() => router.push('/account')}
             />
         </div>
     </footer>
